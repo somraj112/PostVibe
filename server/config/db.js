9100c913966edcde2ed785f057d484a3fd742a75
somraj112 <somrajnandi112@gmail.com>
2025-12-08 04:12:37 +0530

    deployment 3.0

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log("DB Connected...");
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected ✅");
+  } catch (err) {
+    console.error("MongoDB connection failed ❌");
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
