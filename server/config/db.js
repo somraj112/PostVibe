6f8f59fbea49741aa54fb9eb6e69a369e4304d21
somraj112 <somrajnandi112@gmail.com>
2026-09-25 20:06:21 +0530

    optimised the db by using the existing db connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,34 @@
 const mongoose = require("mongoose");
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
 const connectDB = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGO_URI).then((mongoose) => {
+      console.log("MongoDB Connected ✅");
+      return mongoose;
+    });
+  }
+
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected ✅");
+    cached.conn = await cached.promise;
+    return cached.conn;
   } catch (err) {
+    cached.promise = null;
     console.error("MongoDB connection failed ❌");
     console.error(err.message);
-    process.exit(1);
+    throw err;
   }
 };
 
