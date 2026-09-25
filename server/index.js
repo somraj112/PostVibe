c38a92f27e4a38bfa86fc19e1c3e34536ee49ef6
somraj112 <somrajnandi112@gmail.com>
2026-09-25 19:39:31 +0530

    optimised the coverpage by adding options

--- server/index.js
+++ server/index.js
@@ -5,18 +5,22 @@ const router = require("./routes");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-
 dotenv.config();
+
 const app = express();
-connectDB();
-app.use(cors({
+
+const corsOptions = {
   origin: "https://postvibe-sigma.vercel.app",
-  credentials: true
-}));
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions)); 
 
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", router);
+connectDB();
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
